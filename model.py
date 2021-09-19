547d116bb1c58ce9e9d4f2b7bc00aece2009ba44
Sohag Das Sourav <sdsouravg1580@gmail.com>
2021-09-19 16:17:37 +0600

    update

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 import numpy as np
 
-X = np.array([1, 2])  # as an example
+X = np.array([1, 2]).reshape(2,1)  # as an example
 
 input_layer = tf.keras.Input(shape=(X.shape))
 hidden_layer = tf.keras.layers.Dense(4, activation='relu')(input_layer)
